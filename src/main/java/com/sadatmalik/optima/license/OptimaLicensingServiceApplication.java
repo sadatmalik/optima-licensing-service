cc55a380b44d57fd2b4fc0e7eb6874b3a5177bc1
sadat.malik <sadat.malik@me.com>
2022-03-30 17:45:21 +0100

    initial project files

---
+++ src/main/java/com/sadatmalik/optima/license/OptimaLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.optima.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimaLicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimaLicensingServiceApplication.class, args);
+	}
+
+}
