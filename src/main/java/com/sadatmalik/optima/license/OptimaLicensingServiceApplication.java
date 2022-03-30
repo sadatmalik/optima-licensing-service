b0e621045576d6c19e37360c13a36d66f3363026
sadat.malik <sadat.malik@me.com>
2022-03-30 17:59:14 +0100

    updated pom and bootstrap class docs

--- src/main/java/com/sadatmalik/optima/license/OptimaLicensingServiceApplication.java
+++ src/main/java/com/sadatmalik/optima/license/OptimaLicensingServiceApplication.java
@@ -3,6 +3,12 @@ package com.sadatmalik.optima.license;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The bootstrap class for the entire microservice. Core initialization logic for the
+ * service should be placed in this class.
+ *
+ * @author sadatmalik
+ */
 @SpringBootApplication
 public class OptimaLicensingServiceApplication {
 
